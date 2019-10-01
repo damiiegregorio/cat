0cc34b282dcbe486712d8c88a53683d20bdb2f47
damiiegregorio <damiiekeith@gmail.com>
2019-10-01 17:54:53 +0800

    Added delete file function.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 #file objects
+import os
 
 def isReadFile(choose_file):
     with open(choose_file, 'r') as f:
@@ -14,9 +15,14 @@ def isWriteContent(choose_file):
     with open(choose_file, 'a+') as f:
         f.write(file_content)
 
+def isDeleteFile(choose_file):
+    os.remove(choose_file)
+    print("{} file deleted.".format(choose_file))
+
 print("Read a file    --- 1")
 print("Add a file     --- 2")
 print("Add content    --- 3")
+print("Delete content --- 4")
 choice = input("Enter your choice: ")
 choose_file = input("Filename: ")
 
@@ -27,5 +33,7 @@ elif choice == "2":
     print("{} file created.".format(choose_file))
 elif choice == "3":
     ans = isWriteContent(choose_file)
+elif choice == "4":
+    ans = isDeleteFile(choose_file)
 else:
     print("Error")
